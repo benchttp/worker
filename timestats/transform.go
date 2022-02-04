848d725d554b5e35a1720873e0affb8d7d3420f0
moreirathomas <moreirathomas97@gmail.com>
2022-02-04 21:11:21 +0100

    feat: implement Transformer and Floater interface
    - instanciate a Transformer by providing a slice of structs implementing Floater

---
+++ timestats/transform.go
@@ -0,0 +1,39 @@
+package timestats
+
+import (
+	"fmt"
+	"reflect"
+
+	"github.com/montanaflynn/stats"
+)
+
+// Floater is the interface to get a float64 value from any struct.
+type Floater interface {
+	Float() float64
+}
+
+type Transformer struct {
+	Floats stats.Float64Data
+}
+
+// NewTransformer returns a transformer loaded with floats retrieved from x.
+// x is a slice of structs implementing timestats.Floater. Returns an error
+// if x is not a slice or if its elements do not implement timestats.Floater.
+func NewTransformer(x interface{}) (*Transformer, error) {
+	s := reflect.ValueOf(x)
+	if s.Kind() != reflect.Slice {
+		return nil, fmt.Errorf("received a non-slice type: %s", reflect.TypeOf(x))
+	}
+
+	floats := make(stats.Float64Data, s.Len())
+
+	for i := 0; i < s.Len(); i++ {
+		v := s.Index(i)
+		f, ok := v.Interface().(Floater)
+		if !ok {
+			return nil, fmt.Errorf("%s must implement timestats.Floater", v.Type())
+		}
+		floats[i] = f.Float()
+	}
+	return &Transformer{Floats: floats}, nil
+}
---
+++ timestats/transform_test.go
@@ -0,0 +1,71 @@
+package timestats_test
+
+import (
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/montanaflynn/stats"
+
+	"github.com/benchttp/worker/timestats"
+)
+
+type fake struct {
+	time time.Duration
+	any  interface{} //nolint
+}
+
+func (e fake) Float() float64 {
+	return float64(e.time)
+}
+
+func TestNewTransformer(t *testing.T) {
+	for _, testcase := range []struct {
+		name    string
+		raw     interface{}
+		want    stats.Float64Data
+		wantErr bool
+	}{
+		{
+			name:    "return error for non-slice type",
+			raw:     struct{}{},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name:    "return error for slice of structs that do not implement timestats.fake",
+			raw:     []struct{}{{}},
+			want:    nil,
+			wantErr: true,
+		},
+		{
+			name: "transform struct that implements timestats.fake",
+			raw:  []fake{{time: 1 * time.Nanosecond}, {time: 2 * time.Second}},
+			want: stats.Float64Data{1, 2000000000},
+		},
+		{
+			name: "discard unnecessary struct fields",
+			raw:  []fake{{time: 1 * time.Nanosecond, any: struct{}{}}},
+			want: stats.Float64Data{1},
+		},
+	} {
+		t.Run(testcase.name, func(t *testing.T) {
+			transformer, err := timestats.NewTransformer(testcase.raw)
+
+			if testcase.wantErr {
+				if err == nil {
+					t.Fatal("want error, have none")
+				}
+				return // Do not continue test case, transformer == nil.
+			}
+			if err != nil {
+				t.Fatalf("want nil error, got %v", err)
+			}
+
+			got := transformer.Floats
+			if !reflect.DeepEqual(got, testcase.want) {
+				t.Errorf("incorrect transform: want %v, got %v", testcase.want, got)
+			}
+		})
+	}
+}
